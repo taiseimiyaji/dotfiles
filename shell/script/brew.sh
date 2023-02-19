b4ccc0bae77be2386a7a0260bd1c670c884cae35
taisei miyaji <taisei.miyaji4602@gmail.com>
2023-02-19 10:20:15 +0900

    brew add path

--- shell/script/brew.sh
+++ shell/script/brew.sh
@@ -1,6 +1,10 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# PATHを通す必要がある
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/${USER}/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew update
 
 # Install Homebrew bundle software
