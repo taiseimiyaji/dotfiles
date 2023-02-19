d6f7f4fc68f570bba5618ddb549e64c55f0a3173
taisei miyaji <taisei.miyaji4602@gmail.com>
2023-02-19 11:10:19 +0900

    update: zsh setting

--- shell/script/link.sh
+++ shell/script/link.sh
@@ -1,2 +1,9 @@
 ln -s ~/dotfiles/.zshrc ~
 ln -s ~/dotfiles/.gitconfig ~
+
+# zsh フォルダがない場合はmkdir ~/.zsh
+mkdir ~/.zsh
+
+ln -s ~/dotfiles/.zsh/_git ~/.zsh/_git
+ln -s ~/dotfiles/.zsh/git-completion.bash ~/.zsh/git-completion.bash
+ln -s ~/dotfiles/.zsh/git-prompt.sh ~/.zsh/git-prompt.sh
